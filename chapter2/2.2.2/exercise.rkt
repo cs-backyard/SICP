9e1b338cb2ddff3faf4d93a9ea61ccf0d515a447
wsl3 <2350622075@qq.com>
2019-10-09 22:13:56 +0800

    chapter 2

---
+++ chapter2/2.2.2/exercise.rkt
@@ -0,0 +1,27 @@
+; ; begin coding in scheme also in my life (today 2019-10-09)
+; (define x (list "333" 2 3 4 5))
+; (display (car x))
+; (newline)
+; (display (cdr x))
+; (newline)
+
+; length return list's sum(item)
+(define (length items) 
+    (if (null? items)
+        0
+        (+ 1 (length (cdr items))))
+)
+
+; define the print
+(define (print argv split)
+    (cond ((null? argv) (newline))
+        ((not (pair? argv)) (display argv))
+        (else (display (car argv)) (display split) (print (cdr argv) split)))
+)
+
+
+(print (list "display items[[1,2], 3, 4]: " (length (list (list 1 2) 3 4))) " ")
+
+(newline)
+(display (pair? (cdr (list 1 2))))
+(exit)
