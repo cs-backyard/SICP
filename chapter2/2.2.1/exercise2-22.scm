c60f2a32018f5d81d02805c671068f8a08942cbd
unknown <lambdafate@163.com>
2019-10-13 11:45:49 +0800

    add append-num

--- chapter2/2.2.1/exercise2-22.scm
+++ chapter2/2.2.1/exercise2-22.scm
@@ -3,7 +3,22 @@
         list-2
         (cons (car list-1) (append (cdr list-1) list-2)))
 )
+(define (append-num list-1 list-2)
+    (cond 
+        ((null? list-1) list-2)
+        ((not (pair? list-2)) (append-num list-1 (list list-2)))
+        ((not (pair? list-1)) (append-num (list list-1) list-2))
+        (else
+            (cons (car list-1) (append-num (cdr list-1) list-2))
+        )    
+    )
+)
 ; (display (append (list 1 2) (list 3 4)))
+; (display (append-num (list 1 2) 5))
+; (display (append-num 2 5))
+; (display (append-num 1 (cdr (list 1))))
+
+
 
 ; redefine square-list in exercise2-21.scm
 (define (square-list items)
@@ -16,6 +31,6 @@
     )
     (iter-square items (list ))
 )
-
+(newline)
 (display (square-list (list 1 2 -3 4 5)))
 (exit)
